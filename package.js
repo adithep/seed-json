fd0a2ac253fd80437680fd1a8c3d24234244baa5
Adithep SriNarula <badsxx@gmail.com>
2014-07-28 02:24:28 +0700

    mongo

--- package.js
+++ package.js
@@ -7,11 +7,19 @@ Package.on_use(function (api, where) {
   api.add_files([
     'alpha-entity-json/core_team.json',
     'alpha-entity-json/new_company.json',
+    'alpha-entity-json/approaches.json',
     'essential-list-json/_s.json',
     'essential-list-json/keys.json',
+    'essential-list-json/locations.json',
+    'essential-list-json/location_ty.json',
+    'essential-list-json/img.json',
     'web-str-json/_spa.json',
+    'web-str-json/_s_to_load.json',
     'web-str-json/paths.json',
     'web-str-json/_tri_btn.json',
+    'web-str-json/_tri_banners.json',
+    'web-str-json/_tri_s_docs.json',
+    'web-str-json/_tri_media.json',
     'web-str-json/_tri_input.json',
     'web-str-json/_tri.json'], 'server', {isAsset: true});
   api.add_files(['json-control/json-control.coffee','_tri_defaults.js'], 'server');
